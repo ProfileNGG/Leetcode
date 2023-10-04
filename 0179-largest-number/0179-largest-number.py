f235b9f69a62efb9e21cb8f0df519b0e64c28cbb
ProfileNGG <101399856+ProfileNGG@users.noreply.github.com>
2023-10-04 23:56:35 +0900

    Time: 48 ms (47.85%), Space: 16.2 MB (94.57%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,18 +1,18 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        # 비교 함수를 정의합니다.
-        def custom_compare(x, y):
-            xy = str(x) + str(y)
-            yx = str(y) + str(x)
-            return (int(xy) > int(yx)) - (int(xy) < int(yx))
-        
-        # 비교 함수를 사용하여 숫자 리스트를 정렬합니다.
-        nums.sort(key=functools.cmp_to_key(custom_compare), reverse=True)
-        
-        # 0으로만 이루어진 리스트인 경우 '0'을 반환합니다.
-        if nums[0] == 0:
+        if all(num == 0 for num in nums): #0이면 0을 반환
             return "0"
+
+        nums = list(map(str, nums)) #리스트를 문자열로 변환
+        result = []
         
-        # 정렬된 숫자를 문자열로 변환하여 반환합니다.
-        result = "".join(map(str, nums))
-        return result
+        while nums:
+            maxnum = "0"  # 초기값 설정
+            for num in nums:
+                if num + maxnum > maxnum + num:
+                    maxnum = num
+            
+            result.append(maxnum)
+            nums.remove(maxnum)
+        
+        return ''.join(result)
