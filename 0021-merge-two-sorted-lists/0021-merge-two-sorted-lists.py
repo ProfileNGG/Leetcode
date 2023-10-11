a5e7508ef3e86e162faac93cf6ef27164654f977
ProfileNGG <101399856+ProfileNGG@users.noreply.github.com>
2023-10-12 06:43:24 +0900

    Time: 37 ms (90.28%), Space: 16.2 MB (66.21%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,16 +5,16 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy=ListNode()
-        tail=dummy
-        while list1 and list2:
-            if list1.val<list2.val:
-                tail.next=list1
-                list1=list1.next
+        if not list1 or not list2:
+            return list1 or list2
+        d = c = ListNode(0) #d와 c 초기화
+        while list1 and list2: #두 리스트중 더 작은 숫자를 연결해줌
+            if list1.val < list2.val: 
+                c.next = list1
+                list1 = list1.next
             else:
-                tail.next=list2
-                list2=list2.next
-            tail=tail.next
-        if list1: tail.next=list1
-        elif list2: tail.next=list2
-        return dummy.next
+                c.next = list2
+                list2 = list2.next
+            c = c.next
+        c.next = list1 or list2
+        return d.next
