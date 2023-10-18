e115f147efc9cd5ac474442a587aad16cff970b2
ProfileNGG <101399856+ProfileNGG@users.noreply.github.com>
2023-10-18 20:40:51 +0900

    Time: 183 ms (5.12%), Space: 20.9 MB (89.17%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -8,9 +8,9 @@ class Solution:
             if len(arr) <= 1:
                 return arr
 
-            mid = len(arr) // 2
-            left = arr[:mid]
-            right = arr[mid:]
+            mid = len(arr) // 2 #여기서 미드가 2일때
+            left = arr[:mid] # 0,1 
+            right = arr[mid:] # 2,3
 
             left = merge_sort(left)  # 왼쪽 부분 리스트를 재귀적으로 정렬
             right = merge_sort(right)  # 오른쪽 부분 리스트를 재귀적으로 정렬
@@ -22,7 +22,7 @@ class Solution:
             result = []
             i = j = 0
 
-            while i < len(left) and j < len(right):
+            while i < len(left) and j < len(right): # 배열의 첫번째 요소를 기준으로 모든 배열을 정렬함
                 if left[i][0] < right[j][0]:
                     result.append(left[i])
                     i += 1
@@ -30,20 +30,19 @@ class Solution:
                     result.append(right[j])
                     j += 1
 
-            result.extend(left[i:])  # 남은 왼쪽 부분 리스트를 추가
-            result.extend(right[j:])  # 남은 오른쪽 부분 리스트를 추가
+            result.extend(left[i:])  # 남은 리스트를 추가
+            result.extend(right[j:])  # 남은 리스트를 추가
             return result
 
         sorted_intervals = merge_sort(intervals)  # 병합 정렬로 구간을 정렬
         
-        merged = [sorted_intervals[0]]
-        for i in range(1, len(sorted_intervals)):
+        merged = [sorted_intervals[0]] #한번 더 감싸서 [[a,b]] 형태로 줌
+        for i in range(1, len(sorted_intervals)):# 0이아닌 1부터 n 까지(처음에 0과 비교함)
             current_interval = sorted_intervals[i]
-            last_merged = merged[-1]
-
-            if current_interval[0] <= last_merged[1]:
+            last_merged = merged[-1] # 머지드 배열의 마지막 요소
+            if current_interval[0] <= last_merged[1]:# 뒷배열의 앞요소가 앞배열의 뒷요소보다 작으면 
                 last_merged[1] = max(last_merged[1], current_interval[1])  # 구간 병합
             else:
-                merged.append(current_interval)  # 겹치지 않는 경우 새로운 구간 추가
+                merged.append(current_interval)  # 겹치지 않는 경우 새로운 배열 추가
 
         return merged  # 병합된 구간 리스트 반환
