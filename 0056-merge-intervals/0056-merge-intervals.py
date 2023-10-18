9563eec87ff164f27089fc1da2743e34b9cb02aa
ProfileNGG <101399856+ProfileNGG@users.noreply.github.com>
2023-10-18 20:05:52 +0900

    Time: 179 ms (5.12%), Space: 20.8 MB (97.89%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -12,10 +12,10 @@ class Solution:
             left = arr[:mid]
             right = arr[mid:]
 
-            left = merge_sort(left)
-            right = merge_sort(right)
+            left = merge_sort(left)  # 왼쪽 부분 리스트를 재귀적으로 정렬
+            right = merge_sort(right)  # 오른쪽 부분 리스트를 재귀적으로 정렬
 
-            return merge(left, right)
+            return merge(left, right)  # 정렬된 부분 리스트를 병합
 
         # 병합 함수
         def merge(left, right):
@@ -30,11 +30,11 @@ class Solution:
                     result.append(right[j])
                     j += 1
 
-            result.extend(left[i:])
-            result.extend(right[j:])
+            result.extend(left[i:])  # 남은 왼쪽 부분 리스트를 추가
+            result.extend(right[j:])  # 남은 오른쪽 부분 리스트를 추가
             return result
 
-        sorted_intervals = merge_sort(intervals)
+        sorted_intervals = merge_sort(intervals)  # 병합 정렬로 구간을 정렬
         
         merged = [sorted_intervals[0]]
         for i in range(1, len(sorted_intervals)):
@@ -42,8 +42,8 @@ class Solution:
             last_merged = merged[-1]
 
             if current_interval[0] <= last_merged[1]:
-                last_merged[1] = max(last_merged[1], current_interval[1])
+                last_merged[1] = max(last_merged[1], current_interval[1])  # 구간 병합
             else:
-                merged.append(current_interval)
+                merged.append(current_interval)  # 겹치지 않는 경우 새로운 구간 추가
 
-        return merged
+        return merged  # 병합된 구간 리스트 반환
