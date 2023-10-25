e90c64f96a4a778f139949f2504d41c86a119d44
ProfileNGG <101399856+ProfileNGG@users.noreply.github.com>
2023-10-26 03:52:48 +0900

    Time: 631 ms (93.1%), Space: 13.6 MB (86.7%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,10 @@
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:        
-        lst=[math.inf] * (amount+1)# 각 금액까지 만드는데 동전이 최소로 얼마나 드는지 저장하는 리스트(무한대로)
-        lst[0]=0 #0원은 동전 안드니까 0으로 설정
-        
+    def coinChange(self, coins, amount):        
+        lst = [amount+1] * (amount+1)
+        lst[0] = 0
         for coin in coins:
-            for i in range(coin, amount+1): #코인에서 어마운트+1 까지의 범위를 반복하면서 갯수 계산
-                #if i-coin>=0: #원래 있던 코드인데 불필요한 것 같아서 일단 주석처리 함
-                lst[i]=min(lst[i], lst[i-coin]+1)#각 금액 i 가 가질  수 있는 최소의 동전갯수 저장
-        
-        return -1 if lst[-1]==math.inf else lst[-1]#lst[-1]은 리스트의 마지막 값을 반환하는 것, 그러므로 어마운트의 동전갯수 리턴
+            for i in range(coin,amount +1):
+                lst[i] = min(lst[i],lst[i-coin]+1)
+
+        return -1 if lst[-1] == (amount + 1) else lst[-1]
+            
