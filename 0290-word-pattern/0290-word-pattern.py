c939b3dd7158d4b00da3caccf2dbb144c6c78f1c
ProfileNGG <101399856+ProfileNGG@users.noreply.github.com>
2023-10-26 02:40:34 +0900

    Time: 18 ms (18.94%), Space: 13.1 MB (93.23%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,6 +1,19 @@
 class Solution(object):
-    def wordPattern(self, pattern, str):
-        s = pattern
-        t = str.split()
-        return map(s.find, s) == map(t.index, t)
-        
+    def wordPattern(self, pattern, word_str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        words = word_str.split(" ")
+        if len(pattern) != len(words):
+            return False
+
+		# use two dictionaries, mapping character / string with index
+        pattern_map, word_map = {}, {}
+        for i in range(len(pattern)):
+            if pattern_map.get(pattern[i], -1) != word_map.get(words[i], -1):
+                return False
+            pattern_map[pattern[i]] = word_map[words[i]] = i
+
+        return True
