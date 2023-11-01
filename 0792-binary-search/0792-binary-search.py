d3b04d73b1d96d273e601c968cf8b11e03cb6968
ProfileNGG <101399856+ProfileNGG@users.noreply.github.com>
2023-11-02 07:53:53 +0900

    Time: 313 ms (5.11%), Space: 17.9 MB (7.95%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -10,24 +10,24 @@ class Solution:
         if start > end:
             return None
         
-        mid = (start + end) 
-        node = Tree(nums[mid], mid)
-        node.left = self.sort(nums, start, mid - 1)
-        node.right = self.sort(nums, mid + 1, end)
+        mid = (start + end) //2 #중간
+        node = Tree(nums[mid], mid) #중간 번호로 노드만들기 
+        node.left = self.sort(nums, start, mid - 1)# l child 미드를 1 빼서 엔드에 넣음
+        node.right = self.sort(nums, mid + 1, end)# r child 미드를 1 더해서 스타트에 넣음
         return node
 
-    def search(self, nums, target):
+    def search(self, nums, target): # 실행하는 함수
         root = self.sort(nums, 0, len(nums) - 1)
-        return self.searchTree(root, target)
+        return self.searchTree(root, target) #루트랑 타겠을 인자로 넣음
 
     def searchTree(self, node, target):# 인덱스 찾기
         if not node:
             return -1
         
-        if node.val == target:
+        if node.val == target: #밸류가 타겟이랑 같으면 인덱스 리턴
             return node.index
         
-        if target < node.val:
+        if target < node.val: # 타겟이 더 작으면 l차일드로 찾고 크면 r차일드 찾기
             return self.searchTree(node.left, target)
         else:
             return self.searchTree(node.right, target)
